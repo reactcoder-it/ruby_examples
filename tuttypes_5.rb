cf97bf3a4c16169d8f3b497fa1506a5e568181ca
Vadim Pashaev <vmp@live.ru>
2013-05-24 17:20:41 +0400

    Добавлен пример с несколькими операторами в управляющей последовательности в строке.

--- tuttypes_5.rb
+++ tuttypes_5.rb
@@ -4,3 +4,9 @@ puts 'That\'s right'
 puts "Seconds/day: #{24*60*60}"
 puts "#{'Ho! '*3}Merry Christmas!"
 puts "This is line #$."
+
+puts "now is #{ def the(a)
+                  'the ' + a
+                end
+                the('time')
+              } for all good coders..."
